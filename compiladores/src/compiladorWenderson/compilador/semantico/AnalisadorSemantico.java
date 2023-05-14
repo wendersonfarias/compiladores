cb88e27034278fd7ca2ed1f0ef373be794ad144a
Wendersonfarias <wendersonpfarias@gmail.com>
2023-05-13 23:54:37 -0400

    criando analisador semantico

--- compiladores/src/compiladorWenderson/compilador/semantico/AnalisadorSemantico.java
+++ compiladores/src/compiladorWenderson/compilador/semantico/AnalisadorSemantico.java
@@ -1,68 +1,91 @@
 package compiladorWenderson.compilador.semantico;
 
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.Map;
 
-import compiladorWenderson.compilador.TokensLinhaColunaLog;
+import compiladorWenderson.compilador.Token;
 
 public class AnalisadorSemantico {
 
-	List<String> tokens= new Stack<String>();
-	List<String> linhaColuna= new Stack<String>();
+	private Map<String, Integer> tabelaSimbolos; // Tabela de símbolos para armazenar as variáveis
 	
-	TokensLinhaColunaLog tokensLinhaColunaLog = new TokensLinhaColunaLog();
-	
+	private ArrayList<Token> listaToken;
 	
 	
-	public AnalisadorSemantico(TokensLinhaColunaLog tokensLinhaColunaLog) {
-		this.tokensLinhaColunaLog = tokensLinhaColunaLog;
+
+	public AnalisadorSemantico(ArrayList<Token> listaToken) {
+		tabelaSimbolos = new HashMap<>();
+		this.listaToken = listaToken;
 	}
 
-
-	void analisaSemantica() {
+	
+	public ArrayList<String> analiseSemantica() {
+		ListIterator<Token> iterador = getListaToken().listIterator();
 		
-		TabelaDeSimbolos tabela = new TabelaDeSimbolos();
-
-		do {
-			if(tokens.peek().equals("var") || tokens.peek().equals("numero") ) {
-				copia = tokens.pop();
-				
-				if(tokens.peek().equals("var")) {
-					
-					if(tokens.peek().equals("identificador")) {
-						tabela.adicionarSimbolo(tokens.pop(), 0);
-					}else {
-						System.out.println("Erro semantico "+ tokens.pop() + "inesperado");
-					}
-					
-				}else {
-					if(tokens.peek().equals("identificador")) {
-						if(tabela.verificaDeclarado(tokens.pop())) {
-							
-						}	
-					}
-				}
-			}
-		}while(!tokens.empty());
+		while (iterador.hasNext()) {
+		    Token token = iterador.next();
+		    
+		    if(token.getToken().equals("var")) {
+		    	if(iterador.hasNext()) {
+		    		token = iterador.next();
+		    		if(token.getToken().equals("id") && iterador.hasNext() && iterador.next().getToken().equals(";")) {
+		    			analisarDeclaracaoVariavel(token.getLexema());
+		    		}else {
+		    			token = iterador.previous();
+		    			
+		    		}
+		    	}
+		    }
+		}
+		
+		return null;
+		
 	}
 	
 	
-	public void obtemTokens() throws IOException {
-		Stack<String> linhasTokens = new Stack<String>(); 
-		Stack<String> LinhasColunas = new Stack<String>(); 
-		
 	
-		linhasTokens.addAll(tokensLinhaColunaLog.getToken());
-		
-		LinhasColunas.addAll(tokensLinhaColunaLog.getLinhaColuna());
-		
-		
-		Collections.reverse(linhasTokens);
-		Collections.reverse(LinhasColunas);
-		
-		this.tokens = linhasTokens;
-		this.linhaColuna = LinhasColunas;
-		
+	
+	public void analisarDeclaracaoVariavel(String nomeVariavel) {
+	        if (tabelaSimbolos.containsKey(nomeVariavel)) {
+	            // Declaração duplicada de variável
+	            System.out.println("Erro: Variável " + nomeVariavel + " já foi declarada anteriormente.");
+	        } else {
+	            tabelaSimbolos.put(nomeVariavel, 0); // Adiciona a variável na tabela de símbolos com um valor inicial de 0
+	        }
 	}
+	
+	public void analisarReferenciaVariavel(String nomeVariavel) {
+        if (!tabelaSimbolos.containsKey(nomeVariavel)) {
+            // Variável não declarada
+            System.out.println("Erro: Variável " + nomeVariavel + " não foi declarada anteriormente.");
+        }
+    }
+	
+	public void analisarDivisaoPorZero(int divisor) {
+        if (divisor == 0) {
+            System.out.println("Erro: Divisão por zero encontrada.");
+        }
+    }
+	
+	public Map<String, Integer> getTabelaSimbolos() {
+		return tabelaSimbolos;
+	}
+
+	public void setTabelaSimbolos(Map<String, Integer> tabelaSimbolos) {
+		this.tabelaSimbolos = tabelaSimbolos;
+	}
+
+	public ArrayList<Token> getListaToken() {
+		return listaToken;
+	}
+
+	public void setListaToken(ArrayList<Token> listaToken) {
+		this.listaToken = listaToken;
+	}
+	
+	
+
 }
